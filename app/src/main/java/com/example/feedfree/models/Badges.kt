36b0c1130f037d858fcc40a2c0f5132d3a356930
hirooo :) <plumix01@gmail.com>
2026-07-09 17:12:10 +0200

    feat(Data): added Badges to the User Profile

---
+++ app/src/main/java/com/example/feedfree/models/Badges.kt
@@ -0,0 +1,7 @@
+package com.example.feedfree.models
+
+data class Badges(
+    val id: Int,
+    val name: String,
+    val badgesUrl: String? = null
+)
--- app/src/main/java/com/example/feedfree/models/User.kt
+++ app/src/main/java/com/example/feedfree/models/User.kt
@@ -1,9 +1,10 @@
 package com.example.feedfree.models
 
 data class User(
-    val id: String, // Identificatore univoco essenziale
+    val id: String,
     val name: String,
     val username: String,
     val points: Int,
-    val avatarUrl: String? = null // Opzionale, utile per la UI
+    val avatarUrl: String? = null,
+    val badges: List<Badges> = emptyList()
 )

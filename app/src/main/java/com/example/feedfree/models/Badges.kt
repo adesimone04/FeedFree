e931b927faea5885470d65e757de61b5ad7c1d3e
hirooo :) <plumix01@gmail.com>
2026-07-10 11:09:21 +0200

    feat(Data): added Tier to Badges.kt

--- app/src/main/java/com/example/feedfree/models/Badges.kt
+++ app/src/main/java/com/example/feedfree/models/Badges.kt
@@ -3,5 +3,13 @@ package com.example.feedfree.models
 data class Badges(
     val id: Int,
     val name: String,
+    val type: Tier,
     val badgesUrl: String? = null
 )
+
+enum class Tier{
+    BRONZE,
+    SILVER,
+    GOLD,
+    PLATINUM
+}
